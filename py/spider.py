b4dc7e87245dbebee8c403415fe013b81914b6e8
yuan72111 <yuan41330@gmail.com>
2026-04-16 12:53:26 +0800

    Add files via upload

---
+++ py/spider.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www1.pu.edu.tw/~tcyang/course.html"
+Data = requests.get(url)
+Data.encoding = "utf-8"
+#print(Data.text)
+sp = BeautifulSoup(Data.text, "html.parser")
+result=sp.select(".team-box a")
+
+for i in result:
+	print(i.text)
+	print()
--- web.py
+++ web.py
@@ -32,26 +32,56 @@ def index():
     link += "<a href=/welcome?u=黃柏源&d=靜宜資管&c=資訊管理導論>Get傳值</a><hr>"
     link += "<a href=/account>POST傳值</a><hr>"
     link += "<a href=/math>次方與根號計算</a><hr>"
-    link += "<a href=/read>讀取Firestore資料</a><br>"
-    link += "<a href=/read2>讀取Firestore資料(根據姓名關鍵字:楊)</a><br>"
-    link += "<a href=/read3>讀取Firestore資料(根據姓名關鍵字:input)</a><br>"
+    link += "<a href=/read>讀取Firestore資料</a><hr>"
+    link += "<a href=/read2>讀取Firestore資料(根據姓名關鍵字:楊)</a><hr>"
+    link += "<a href=/read3>讀取Firestore資料(根據姓名關鍵字:input)</a><hr>"
+    link += "<a href=/spider1>爬取子青老師本學期課程</a><hr>"
     return link
+
+@app.route("/spider1")
+def spider1():
+    Result = ""
+    url = "https://www1.pu.edu.tw/~tcyang/course.html"
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+    #print(Data.text)
+    sp = BeautifulSoup(Data.text, "html.parser")
+    result=sp.select(".team-box a")
+
+    for i in result:
+        Result += i.text + i.get("href") + "<br>"
+    return Result
+
+from flask import Flask, request
 
 @app.route("/read3")
 def read3():
+    keyword = request.args.get("keyword")
+    
     Result = ""
-    keyword = input("請輸入姓名關鍵字:")
     db = firestore.client()
-    collection_ref = db.collection("靜宜資管")    
+    collection_ref = db.collection("靜宜資管")
     docs = collection_ref.get()
-    for doc in docs:    
+
+    if not keyword:
+        return """
+        <h1>靜宜資管老師查詢</h1>
+        <form action="/read3" method="get">
+            請輸入老師姓名關鍵字：<input type="text" name="keyword">
+            <button type="submit">查詢</button>
+        </form>
+        <br><a href="/">返回首頁</a>
+        """
+
+    for doc in docs:
         teacher = doc.to_dict()
-        if keyword in teacher["name"]:
+        if keyword in teacher.get("name", ""):
             Result += str(teacher) + "<br>"
 
     if Result == "":
-        Result = "抱歉,查無此關鍵字姓名之老師資料"
-    return Result
+        Result = "抱歉，查無此關鍵字姓名之老師資料"
+    
+    return f"<h1>查詢結果</h1>{Result}<br><a href='/read3'>重新查詢</a>"
 
 @app.route("/read2")
 def read2():
