c7b2d7f33781ddac10b299d869709898ff12b6fa
yuan72111 <yuan41330@gmail.com>
2026-05-14 11:32:31 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -52,8 +52,12 @@ def webhook():
     req = request.get_json(force=True)
     # fetch queryResult from json
     action =  req["queryResult"]["action"]
-    msg =  req["queryResult"]["queryText"]
-    info = "我是黃柏源設計的機器人,動作：" + action + "； 查詢內容：" + msg
+    #msg =  req["queryResult"]["queryText"]
+    #info = "我是黃柏源設計的機器人,動作：" + action + "； 查詢內容：" + msg
+    if (action == "rateChoice"):
+        rate =  req["queryResult"]["parameters"]["rate"]
+        info = "我是黃柏源設計的機器人,您選擇的電影分級是：" + rate
+        
     return make_response(jsonify({"fulfillmentText": info}))
 
 
